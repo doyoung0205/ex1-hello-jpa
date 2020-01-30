dbe6aa87d1e98de880350fb55245eb9cbb02dbfa
Cat-Hanbit <doyoung0205@naver.com>
2020-01-30 10:33:00 +0900

    create member entity JpaMain add select member code with transaction
    add .gitignore

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,6 +12,28 @@ public class JpaMain {
         // entityManagerFactory -> emf
         final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        final EntityManager em = emf.createEntityManager();
+
+        final EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            em.persist(member);
+
+            tx.commit();
+
+        } catch (Exception e) {
+
+            tx.rollback();
+        } finally {
+
+            em.close();
+        }
 
         emf.close();
     }
