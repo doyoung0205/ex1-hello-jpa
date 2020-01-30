00a0c05e3125c02c6cf4b41cfc6b2aa02af1543a
KDY <doyoung0205@naver.com>
2020-01-30 11:10:05 +0900

    add README.md add member crud

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,17 +15,47 @@ public class JpaMain {
         final EntityManager em = emf.createEntityManager();
 
         final EntityTransaction tx = em.getTransaction();
-        tx.begin();
 
         try {
 
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+            final long memberId = 3L;
+            final String memberName = "member_name";
 
-            em.persist(member);
 
-            tx.commit();
+            /** 저장 */
+            {
+                tx.begin();
+                final Member member = new Member();
+                member.setId(memberId);
+                member.setName(memberName);
+
+                // member 저장
+                em.persist(member);
+                tx.commit();
+            }
+
+            /** 조회 및 수정*/
+            {
+                tx.begin();
+                // 저장한 member 조회 -
+                final Member findMember = em.find(Member.class, memberId);
+                System.out.println(findMember.getId().compareTo(memberId) == 0);
+                System.out.println(findMember.getName().equals(memberName));
+
+                // 찾은 member 수정 더티 체킹
+                findMember.setName("changedName");
+                tx.commit();
+            }
+
+            /** 조회 및 삭제 */
+            {
+
+                tx.begin();
+                final Member findMember = em.find(Member.class, memberId); // 이때는 캐싱한 객체가 반환됨
+                em.remove(findMember);
+                tx.commit();
+            }
+
 
         } catch (Exception e) {
 
